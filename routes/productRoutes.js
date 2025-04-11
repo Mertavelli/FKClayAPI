2556bf3139879fc779e59aba534b2d1e112e8f35
Mertavelli <Louiskarakas.bw@gmail.com>
2025-04-11 12:45:29 +0200

    Stripe integration

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,12 @@ import express from "express";
 import multer from "multer";
 import path from "path";
 import Product from "../models/Product.js";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+dotenv.config();
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 
 const router = express.Router();
 
@@ -58,6 +64,37 @@ router.post("/", upload.single("image"), async (req, res) => {
         res.status(201).json(product);
     } catch (error) {
         res.status(500).json({ message: "Fehler beim Hochladen", error });
+    }
+});
+
+// Checkout-Session erstellen (Stripe)
+router.post("/checkout", async (req, res) => {
+    console.log("checkout")
+    try {
+        const items = req.body.items;
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: items.map((item) => ({
+                price_data: {
+                    currency: "eur",
+                    product_data: {
+                        name: item.name,
+                        images: [item.image],
+                    },
+                    unit_amount: item.price * 100,
+                },
+                quantity: item.quantity,
+            })),
+            mode: "payment",
+            success_url: `${process.env.CLIENT_URL}/`,
+            cancel_url: `${process.env.CLIENT_URL}/`,
+        });
+
+        res.status(200).json({ id: session.id });
+    } catch (error) {
+        console.error("Stripe Checkout Error:", error);
+        res.status(500).json({ error: error.message });
     }
 });
 
